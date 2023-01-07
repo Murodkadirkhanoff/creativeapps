7d34925ca173a14ba72eff214fceaf863817f405
Murodkadirkhanoff <37834093+Murodkadirkhanoff@users.noreply.github.com>
2023-01-07 13:39:27 +0500

    sad

---
+++ public/defines.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+define('FACEBOOK_APP_ID', '644631963731915');
+define('FACEBOOK_APP_SECRET', 'dfbb6e1d52255c8c5ce5ce6b3245c51e');
+define('FACEBOOK_REDIRECT_URI', 'https://creativeapps.uz/obtaining_access_token.php');
+define('ENDPOINT_BASE', 'https://graph.facebook.com/v15.0/');
+
+// accessToken
+$accessToken = 'EAAJKShM1h8sBAAflAFW5cxtCPjXhmL8cZBPRSEzqZA7lf1gUKrEGY5WDuncBeu87a0q2YPZCZAgcKV4ewcRREMIZAWq7bFnOby9EHgdNAIuSPZACMzt3Hkzx8fp6uiM6hzVWIhGhrfIrFgw73gUed3zfKIEkSEWGQFtURlgDmOtAsupzUobimLwrZBNiuIiBqomUfyf5bZC1myuJBJvYbBt6yURS8ikK3w7wTEWgPyZA6Oogf0WOHZBBWq';
+
+
+// page id
+$pageId = '101952039378381';
+
+// instagram business account id
+$instagramAccountId = '17841401733631544';
